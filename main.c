9c63f4ba1bb38f9dc47fbd283bd84b3dec5f43f6
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-12 16:36:15 -0500

    tercer commit

--- main.c
+++ main.c
@@ -5,5 +5,9 @@ int main() {
     int suma;
     printf("Ingrese un número entero positivo: ");
     scanf("%d", &n);
+    if (n < 0) {
+        printf("El número ingresado no es válido. Debe ser un número entero positivo.\n");
+    }
+        
     
 }
