913a1c65c241017489fc6d414c94eefe8bb42142
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-12 16:37:05 -0500

    cuarto commit

--- main.c
+++ main.c
@@ -7,7 +7,11 @@ int main() {
     scanf("%d", &n);
     if (n < 0) {
         printf("El número ingresado no es válido. Debe ser un número entero positivo.\n");
+    } else {
+        suma = (n * (n + 1)) / 2;
+        printf("La suma de Gauss para %d es: %d\n", n, suma);
     }
-        
+
+    return 0; 
     
 }
